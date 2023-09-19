99b18fd422f19b03f392ae5e034f3990693ccbbb
Luca Oliveira Lima <104513017+Lucx33@users.noreply.github.com>
2023-09-19 11:11:10 -0300

    Update ex1.c
    Ta funcionando, (acho)

--- Lab2/ex1.c
+++ Lab2/ex1.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/shm.h>
-#include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -10,22 +9,47 @@
 
 int main()
 {
-    int segmento, *t;
+    int segmento, segmento2, *t, *somaTot;
+    
     segmento = shmget(IPC_PRIVATE, TAM * sizeof(int), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
     t = (int *) shmat(segmento, 0, 0);
-    for (int i = 0; i < TAM; i++){
+    
+    segmento2 = shmget(IPC_PRIVATE, 10 * sizeof(int), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+    somaTot = (int *) shmat(segmento2, 0, 0);
+    
+    for (int i = 0; i < TAM; i++) {
         t[i] = 5;
     }
-    for (int i = 0; i <10; i++){
-        if (fork() == 0){
-            int somaTot = 0;
-            for (int j = 0; j < 1000; j++){
-                int pos = i*1000 + j;
+
+    for (int i = 0; i <10; i++) {
+        if (fork() == 0) {
+            int total = 0;
+            for (int j = 0; j < 1000; j++) {
+                int pos = i * 1000 + j;
                 t[pos] *= 2;
-                somaTot += t[pos]; 
+                total += t[pos];
             }
+            somaTot[i] = total;
+            shmdt(t);  
             exit(0);
-        } 
+        }
     }
+
+    // Parent waits for all children to complete
+    for (int i = 0; i < 10; i++) {
+        wait(NULL);
+    }
+    int final = 0;
+    for (int i = 0; i < 10; i++) {
+        final += somaTot[i];
+    }
+
+    printf("Total: %d\n", final);
+
+    shmdt(t);
+    shmctl(segmento, IPC_RMID, NULL);
+    shmdt(somaTot);
+    shmctl(segmento2, IPC_RMID, NULL);
+
     return 0;
 }
